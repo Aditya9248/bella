c97b4347ce894cc6b05813a2f2da1f2e8475dfd8
Aditya <adithakare40@gmaail.com>
2026-07-24 12:51:31 +0000

    Added Kubernetes ConfigMap, Secret and automated deployment

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Bella!');
+    res.send('Bella Application Version 2');
 });
 
 app.listen(PORT, () => {
